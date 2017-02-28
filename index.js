ddd54f93ea323d779515cd5c0b61035cfc131d95
Evan Summers <evan.summers@gmail.com>
2017-02-28 10:26:00 +0000

    publish

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+const assert = require('assert');
 const lodash = require('lodash');
 
-module.exports = (keys, fn, defaultObject = {}) => keys.reduce((object, key, index) => {
-    object[key] = fn(key, index);
-    return object;
-}, defaultObject);
+module.exports = (keys, fn, defaultObject = {}) => {
+    assert(lodash.isArray(keys), 'reduce keys array');
+    return keys.reduce((object, key, index) => {
+        object[key] = fn(key, index);
+        return object;
+    }, defaultObject);
+}
